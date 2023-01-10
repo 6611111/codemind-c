b810074c9f34beae7db5bd2c4cba17757a1c998c
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-01-10 13:00:20 +0530

    #include<stdio.h> int main() {     int m,p,c,r,s=0,o=0,k,e;     scanf("%d",&m);     p=m*m;     c=m;     while(c!=0)     {         r=c%10;         s=s*10+r;         c=c/10;     }     k=s*s;     while(k!=0)     {         e=k%10;         o=o*10+e;         k=k/10;     }     if(o==p)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Adam_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int m,p,c,r,s=0,o=0,k,e;
+    scanf("%d",&m);
+    p=m*m;
+    c=m;
+    while(c!=0)
+    {
+        r=c%10;
+        s=s*10+r;
+        c=c/10;
+    }
+    k=s*s;
+    while(k!=0)
+    {
+        e=k%10;
+        o=o*10+e;
+        k=k/10;
+    }
+    if(o==p)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
