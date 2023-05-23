4735bd01593d6b0fff976aa74259753f3878263a
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-05-22 22:06:20 -0700

    #include<stdio.h> int main() {     int r,c,i,j,s1=0,s2=0;     scanf("%d%d",&r,&c);     int a[r][c];     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i%2==0)             {                 s1=s1+a[i][j];             }             else if(i%2!=0)             {                 s2=s2+a[i][j];             }         }     }     printf("%d %d",s1,s2); }

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,s1=0,s2=0;
+    scanf("%d%d",&r,&c);
+    int a[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i%2==0)
+            {
+                s1=s1+a[i][j];
+            }
+            else if(i%2!=0)
+            {
+                s2=s2+a[i][j];
+            }
+        }
+    }
+    printf("%d %d",s1,s2);
+}
